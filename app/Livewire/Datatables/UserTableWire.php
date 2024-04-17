ba00e0ad44d6619dd6e5de0494bf1e18f6068d0f
permadihendra <26792223+permadihendra@users.noreply.github.com>
2024-04-17 14:20:34 +0700

    User table add scopeSearch function

--- app/Livewire/Datatables/UserTableWire.php
+++ app/Livewire/Datatables/UserTableWire.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire\Datatables;
 
+use Livewire\WithPagination;
 use Livewire\Component;
+
+
+use App\Models\User;
+
 
 class UserTableWire extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public $search = '';
+    public $perPage = 10;
+
     public function render()
     {
-        return view('livewire.datatables.user-table-wire');
+        return view('livewire.datatables.user-table-wire', [
+            'users' => User::search($this->search)->paginate($this->perPage),
+        ]);
     }
 }
--- resources/views/livewire/datatables/user-table-wire.blade.php
+++ resources/views/livewire/datatables/user-table-wire.blade.php
@@ -1,17 +1,17 @@
 <div class="card">
-    <div class="card-header bg-light">
+    <div class="card-header bg-body-light">
         <div class="row">
             <div class="col-sm-4">
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon1"><i class="fa fa-search"></i></span>
-                    <input type="text" class="form-control" placeholder="Search" aria-label="Search"
-                        aria-describedby="basic-addon1">
+                    <input wire:model.live.debounce.300ms="search" type="text" class="form-control"
+                        placeholder="Search" aria-label="Search" aria-describedby="basic-addon1">
                 </div>
             </div>
 
             <div class="col-sm-auto row ms-auto">
                 <div class="col-auto">
-                    <label for="usertype">User Type : </label>
+                    <label for="usertype" class="col-form-label">User Type : </label>
                 </div>
                 <div class="col-auto">
                     <select id="usertype" class="form-select" aria-label="Default select example">
@@ -26,36 +26,72 @@
 
         </div>
     </div>
-    <div class="card-body">
-        <table class="table table-hover">
+    <div class="card-body overflow-x-scroll">
+        <table class="table table-hover fs-6">
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">email</th>
+                    <th scope="col">Role</th>
+                    <th scope="col">Created</th>
+                    <th scope="col">Last Update</th>
+                    <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+                @foreach ($users as $user)
+                    <tr :key={{ $user->id }}>
+                        <td>{{ ($users->currentPage() - 1) * $users->perPage() + $loop->iteration }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>
+                            @if ($user->is_admin)
+                                <small class="badge bg-success">Admin</small>
+                            @else
+                                <small class="badge bg-secondary">User</small>
+                            @endif
+                        </td>
+                        <td>{{ $user->created_at }}</td>
+                        <td>{{ $user->updated_at }}</td>
+                        <td>
+                            <div class="row">
+                                <button class="btn btn-secondary"> : </button>
+                                <button class="btn btn-danger"> X </button>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+
             </tbody>
         </table>
+
+        <div>
+            <div class="col-sm-4">
+                <div class="row">
+                    <div class="col-5">
+                        <label for="usertype" class="col-form-label">Per page : </label>
+                    </div>
+                    <div class="col-5">
+                        <select wire:model.change="perPage" id="usertype" class="form-select"
+                            aria-label="Default select example">
+                            <option value="10">10</option>
+                            <option value="20">20</option>
+                            <option value="30">30</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-auto">
+                {{ $users->links() }}
+            </div>
+
+
+        </div>
+
     </div>
-    <div class="card-footer"></div>
+
 </div>
