487d797faa62107be6f587648a15f920fb272d68
permadihendra <26792223+permadihendra@users.noreply.github.com>
2024-04-30 12:40:35 +0700

    add latest article and popular articles on dashboard

--- app/Livewire/Backend/Dashboard.php
+++ app/Livewire/Backend/Dashboard.php
@@ -11,12 +11,13 @@ use App\Models\Category;
 class Dashboard extends Component
 {
 
-    public $total_articles, $total_categories, $latest_articles;
+    public $total_articles, $total_categories, $latest_articles, $popular_articles;
 
     public function boot(){
         $this->total_articles = Article::count();
         $this->total_categories = Category::count();
-        $this->latest_articles = Article::orderBy('updated_at')->take(10)->get();
+        $this->latest_articles = Article::orderBy('updated_at', 'DESC')->take(5)->get();
+        $this->popular_articles = Article::where('status', 1)->orderBy('views', 'DESC')->take(5)->get();
     }
 
     #[Layout('components.layouts.template')]
--- resources/views/livewire/backend/dashboard.blade.php
+++ resources/views/livewire/backend/dashboard.blade.php
@@ -30,39 +30,84 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-6">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Title</th>
-                        <th>Category</th>
-                        <th>Status</th>
-                        <th>Last Update</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    @foreach ($latest_articles as $article)
+        <div class="col-lg-6 overflow-auto">
+            <h4>Latest Article</h2>
+                <table class="table table-bordered table-striped">
+                    <thead>
                         <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $article->title }}</td>
-                            <td>{{ $article->category->name }}</td>
-                            <td>
-                                @if ($article->status == 1)
-                                    <span class="badge bg-success text-white">Published</span>
-                                @else
-                                    <span class="badge bg-danger text-white">Draft</span>
-                                @endif
-                            </td>
-                            <td>{{ $article->updated_at }}</td>
-                            <td></td>
+                            <th>No</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Last Update</th>
+                            <th>Action</th>
                         </tr>
-                    @endforeach
+                    </thead>
+                    <tbody>
 
-                </tbody>
-            </table>
+                        @foreach ($latest_articles as $article)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $article->title }}</td>
+                                <td>{{ $article->category->name }}</td>
+                                <td>
+                                    @if ($article->status == 1)
+                                        <span class="badge bg-success text-white">Published</span>
+                                    @else
+                                        <span class="badge bg-danger text-white">Draft</span>
+                                    @endif
+                                </td>
+                                <td>{{ $article->updated_at }}</td>
+                                <td>
+                                    <a href="{{ url('articles/edit/' . $article->id) }}"
+                                        class="btn btn-sm btn-outline-secondary">Details</a>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
+        </div>
+
+        <div class="col-lg-6 overflow-auto">
+            <h4>Popular Article</h2>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Last Update</th>
+                            <th>Views</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        @foreach ($popular_articles as $popular)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $popular->title }}</td>
+                                <td>{{ $popular->category->name }}</td>
+                                <td>
+                                    @if ($popular->status == 1)
+                                        <span class="badge bg-success text-white">Published</span>
+                                    @else
+                                        <span class="badge bg-danger text-white">Draft</span>
+                                    @endif
+                                </td>
+                                <td>{{ $popular->updated_at }}</td>
+                                <td>{{ $popular->views }}</td>
+                                <td>
+                                    <a href="{{ url('articles/edit/' . $popular->id) }}"
+                                        class="btn btn-sm btn-outline-secondary">Details</a>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
         </div>
     </div>
 </div>
